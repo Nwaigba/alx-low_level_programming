185dffd6cb74be2d827de5ef9f3842b3b63bae22
Nwaigba <ezehemmanuelval@gmail.com>
2022-12-10 07:41:31 -0800

    Hot fix

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -17,11 +17,11 @@ int main(void)
 	{
 		printf("Last digit of %d is %d and is greater than 5\n", n, l);
 	}
-	else (1 == 0)
+	else
 	{
 		printf("Last digit of %d is %d and is 0\n", n, l);
 	}
-	else
+	else if (1 == 0)
 	{
 		printf("Last digit of %d is %d and is less than 6 and not 0\n", n, l);
 	}
