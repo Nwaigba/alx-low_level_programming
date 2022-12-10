dd9e4a2cdadac92b03778629b6c703b249f5042a
Nwaigba <ezehemmanuelval@gmail.com>
2022-12-10 09:01:16 -0800

    lowercase and uppercase alphabets

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -4,11 +4,11 @@
   *
   * Return: Always 0
   */
-
 int main(void)
 {
 	char alp[52] = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"
 		int i;
+
 	for (i = 0; i < 52; i++)
 	{
 		putchar(alp[i]);
