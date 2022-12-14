ae86c6b818ac1551586470257f702a450a050f63
Nwaigba <ezehemmanuelval@gmail.com>
2022-12-14 03:34:28 -0800

    a function that prints the alphabet, in lowercase, followed by a new line

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,16 +1,17 @@
 #include "main.h"
 
 /**
-  * main -  I sometimes suffer from insomnia. 
-  * And when I can't fall asleep, I play what I call the alphabet game
-  *
-  * Return: Always 0
-  */
-
+ * print_alphabet - Entry point
+ * Description -  I sometimes suffer from insomnia
+ * And when I can't fall asleep
+ *
+ * Return: Always 0
+ */
 void print_alphabet(void)
 {
 	char atoz = 'a';
-	while (atoz <='z')
+
+	while (atoz <= 'z')
 	{
 		_putchar(atoz);
 		atoz++;
