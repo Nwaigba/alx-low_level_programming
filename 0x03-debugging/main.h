09b00813a08b70b550720c936381e3c3d50e8820
Nwaigba <ezehemmanuelval@gmail.com>
2022-12-15 02:00:26 -0800

    file that test that the function positive_or_negative() gives the correct output when given a case of 0

---
+++ 0x03-debugging/main.h
@@ -0,0 +1,15 @@
+#ifndef MAIN_H
+#define MAIN_H
+#include <stdio.h>
+
+/**
+ * main - created functions for debugging errors in my code
+ * Return: Always 0
+ */
+
+void positive_or_negative(int i);
+int largest_number(int a, int b, int c);
+void print_remaining_days(int month, int day, int year);
+int convert_day(int month, int day);
+
+#endif
